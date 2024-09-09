4ffee2e981b8d9bccbc7a553ace14f58b4d3a11f
Emilly Poliane <emilly.carmo@dcx.ufpb.br>
2024-09-09 18:49:40 -0300

    Corrigindo erros

--- src/main/java/br/ufpb/dcx/rodrigor/projetos/Keys.java
+++ src/main/java/br/ufpb/dcx/rodrigor/projetos/Keys.java
@@ -1,14 +1,14 @@
 package br.ufpb.dcx.rodrigor.projetos;
 
 import br.ufpb.dcx.rodrigor.projetos.db.MongoDBConnector;
-import br.ufpb.dcx.rodrigor.projetos.edital.services.EditalService;
+import br.ufpb.dcx.rodrigor.projetos.edital.service.EditalService;
 import br.ufpb.dcx.rodrigor.projetos.participante.services.ParticipanteService;
 import br.ufpb.dcx.rodrigor.projetos.projeto.services.ProjetoService;
 import io.javalin.config.Key;
 
 public enum Keys {
     MONGO_DB(new Key<MongoDBConnector>("mongo-db")),
-    EDITAIS_SERVICE(new Key<EditalService>("editais-service")),
+    EDITAl_SERVICE(new Key<EditalService>("editais-service")),
     PROJETO_SERVICE(new Key<ProjetoService>("projeto-service")),
     PARTICIPANTE_SERVICE(new Key<ParticipanteService>("participante-service"))
     ;
