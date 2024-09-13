8767f81374465841df06ddb8255e0c177f010648
CauaBt <caua.brito@dcx.ufpb.br>
2024-09-13 11:06:22 -0300

    Validação do model Edital

--- src/main/java/br/ufpb/dcx/rodrigor/projetos/edital/model/Edital.java
+++ src/main/java/br/ufpb/dcx/rodrigor/projetos/edital/model/Edital.java
@@ -1,14 +1,16 @@
 package br.ufpb.dcx.rodrigor.projetos.edital.model;
 
 import br.ufpb.dcx.rodrigor.projetos.participante.model.Participante;
-import br.ufpb.dcx.rodrigor.projetos.participante.services.ParticipanteService;
 
-import java.util.List;
-import java.util.Optional;
 
 public class Edital {
     private String id, titulo, data, descricao, calendario, preRequisitos, formInscricao;
     private Participante coordenador;
+
+    private static final int MAX_TITULO_LENGTH = 350;
+    private static final int MAX_DESCRICAO_LENGTH = 1000;
+    private static final int MAX_PREREQUISITOS_LENGTH = 800;
+    private static final int MAX_FORMINSCRICAO_LENGTH = 600;
 
     public Edital(String id, String titulo, String data, String descricao, String calendario, String preRequisitos, String formInscricao, Participante coordenador) {
         this.id = id;
@@ -38,6 +40,8 @@ public class Edital {
     }
 
     public void setTitulo(String titulo) {
+        if(titulo.isBlank()) throw new IllegalArgumentException("O título não pode estar vazio");
+        if(titulo.length() > MAX_TITULO_LENGTH) throw new IllegalArgumentException("O título não pode ter mais de 250 caracteres");
         this.titulo = titulo;
     }
 
@@ -54,6 +58,8 @@ public class Edital {
     }
 
     public void setDescricao(String descricao) {
+        if(descricao.isBlank()) throw new IllegalArgumentException("A descrição não pode estar vazio");
+        if(descricao.length() > MAX_DESCRICAO_LENGTH) throw new IllegalArgumentException("A descrição não pode ter mais de 1000 caracteres");
         this.descricao = descricao;
     }
 
@@ -70,6 +76,8 @@ public class Edital {
     }
 
     public void setPreRequisitos(String preRequisitos) {
+        if(preRequisitos.isBlank()) throw new IllegalArgumentException("Os pré-requisitos não podem estar vazio");
+        if(preRequisitos.length() > MAX_PREREQUISITOS_LENGTH) throw new IllegalArgumentException("Os pré-requisitos não podem ter mais de 800 caracteres");
         this.preRequisitos = preRequisitos;
     }
 
@@ -78,6 +86,8 @@ public class Edital {
     }
 
     public void setFormInscricao(String formInscricao) {
+        if(formInscricao.isBlank()) throw new IllegalArgumentException("O formulário de inscrição não pode estar vazio");
+        if(formInscricao.length() > MAX_FORMINSCRICAO_LENGTH) throw new IllegalArgumentException("O formulário de inscrição não pode ter mais de 600 caracteres");
         this.formInscricao = formInscricao;
     }
 
